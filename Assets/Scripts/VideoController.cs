0206c72f021fd83996f2cfff66cf72b9d6b44b8f
Andrew Gorman <andrew@andrewcgorman.com>
2015-04-01 00:58:29 -0500

    Put the movie into the repo

---
+++ Assets/Scripts/VideoController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VideoController : MonoBehaviour {
+
+	public MovieTexture myMovie;
+
+	void Start () {
+		myMovie.Play ();
+	}
+
+	// Update is called once per frame
+	void OnGUI () {
+		GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height),myMovie ,ScaleMode.StretchToFill);
+		if (Input.GetKeyDown (KeyCode.Return)) {
+			myMovie.Stop();
+			Application.LoadLevel ("Level_1");
+		}
+	}
+
+}
